3e64b3bcb8a934bd8d2b606855fa283b8327a741
CrumblyLiquid <crumblyliquid@gmail.com>
2021-06-19 10:09:19 +0200

    Add prefix alias

--- Redditor/cogs/Settings.py
+++ Redditor/cogs/Settings.py
@@ -13,7 +13,8 @@ class Settings(commands.Cog):
     async def settings(self, ctx):
         pass
 
-    @settings.command(brief="Set command prefix",
+    @settings.command(name="prefix",
+                      brief="Set command prefix",
                       short_doc="Allows you to view or set command prefix",
                       description="Allows you to customize or view your Redditor's command prefix.")
     @commands.guild_only()
@@ -27,6 +28,18 @@ class Settings(commands.Cog):
         await self.bot.pm.a_set(ctx.guild.id, p)
         embed = discord.Embed(title=f"Prefix for {ctx.guild.name}", description=f"Prefix: `{p}`", color=self.bot.main_colour)
         return await ctx.reply(embed=embed, mention_author=False)
+
+    # So we can use r/prefix instead of r/settings prefix
+    @commands.command(name="prefix",
+                      hidden=True,
+                      brief=f"Shortcut for r/settings prefix",
+                      short_doc="Shortcut for r/settings prefix",
+                      description="Shortcut for r/settings prefix")
+    @commands.guild_only()
+    @commands.has_guild_permissions(administrator=True)
+    @commands.cooldown(2, 60, commands.BucketType.user)
+    async def prefix_alias(self, ctx, p: str = None):
+        await ctx.invoke(self.bot.get_command("settings prefix"), p)
 
 def setup(bot):
     bot.add_cog(Settings(bot))
